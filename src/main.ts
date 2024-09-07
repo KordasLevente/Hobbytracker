de05cff5e9b1a865f5c0f4ca8eda7eb2e6d6b418
Kordás Levente <kordas.levente02@gmail.com>
2024-09-07 12:55:46 +0200

    ui complete

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,82 @@
-
 const timer = document.querySelector("#timer")
 const timerControl = document.querySelector("#timerControl")
 const calendar = document.querySelector("#calendar")
+const studyTitle = document.querySelector("#studytitle")
+const hobbyTitle = document.querySelector("#hobbytitle")
+const monthDisplay = document.querySelector("#currentmonth")
+const yearDisplay = document.querySelector("#currentyear")
+const prevMonthButton = document.querySelector("#prevmonth")
+const nextMonthButton = document.querySelector("#nextmonth")
+const shortGoalDisplay = document.querySelector("#shortgoal")
+const longGoalDisplay = document.querySelector("#longgoal")
+
 let timerRunning = false
 let timeSpent = 0
+let focusedDay = new Date()
+let studymode = false
 
 function daysInMonth (month:number, year:number) : number {
     return new Date(year, month, 0).getDate();
 }
 
-function formatTimerOutput(input:number) {
-    return input < 10 ? `0${input}` : input
+function leftpadLowNumber(input:number, padstr : string) {
+    return input < 10 ? `${padstr}${input}` : input
 }
 
-function setup() {
-    let now = new Date()
-    loadCalendar(now.getMonth(), now.getFullYear())
-}
-
-function loadCalendar(month:number, year:number) {
-    let days = daysInMonth(month, year)
+function loadCalendar() {
+    let days = daysInMonth(focusedDay.getMonth(), focusedDay.getFullYear())
     for(let i = 1; i <= days; i++){
+        //diferent colours based on studymode
         let day = document.createElement("div")
         day.classList.add("day")
-        day.textContent = `${i}`
+        day.textContent = `${leftpadLowNumber(i, " ")}`
         calendar.appendChild(day)
     }
 }
 
+function updateCalendar() {
+    updateCalendarHeader()
+    calendar.textContent = ""
+    loadCalendar()
+}
+
+function updateCalendarHeader() {
+    let month : string = focusedDay.toLocaleString('en', { month: 'long' });
+    let year = focusedDay.getFullYear();
+    monthDisplay.textContent = `${month}`
+    yearDisplay.textContent = `${year}`
+}   
+
 function updateTimer() {
     if(!timerRunning) return
-    timeSpent += 1000
+    timeSpent += 10
     var hours = Math.floor((timeSpent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeSpent % (1000 * 60 * 60)) / (1000 * 60))
     var seconds = Math.floor((timeSpent % (1000 * 60)) / 1000)
-    timer.textContent = `${hours}:${formatTimerOutput(minutes)}:${formatTimerOutput(seconds)}`
+    timer.textContent = `${hours}:${leftpadLowNumber(minutes,"0")}:${leftpadLowNumber(seconds,"0")}`
 }
 
 
+
+function switchToStudy() {
+    studymode = true
+    shortGoalDisplay.classList.remove("fg-darkgreen")
+    longGoalDisplay.classList.remove("fg-lightgreen")
+    shortGoalDisplay.classList.add("fg-darkred")
+    longGoalDisplay.classList.add("fg-lightred")
+    studyTitle.classList.remove("notbold")
+    hobbyTitle.classList.add("notbold")
+}
+
+function switchToHobby() {
+    studymode = false
+    shortGoalDisplay.classList.add("fg-darkgreen")
+    longGoalDisplay.classList.add("fg-lightgreen")
+    shortGoalDisplay.classList.remove("fg-darkred")
+    longGoalDisplay.classList.remove("fg-lightred")
+    studyTitle.classList.add("notbold")
+    hobbyTitle.classList.remove("notbold")
+}
 
 timerControl.addEventListener("click", (e) =>{
     if(timerRunning){
@@ -52,5 +91,8 @@ timerControl.addEventListener("click", (e) =>{
 
     }
 })
-setup()
-setInterval(updateTimer,1000)
+
+hobbyTitle.addEventListener("click", switchToHobby)
+studyTitle.addEventListener("click", switchToStudy)
+updateCalendar()
+setInterval(updateTimer,10)
